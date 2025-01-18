8b3f3d1dbf5544c2b358cf0812d0cc2d986a7dc2
LuisRGN <luisrafael1598@gmail.com>
2025-01-18 12:42:02 -0500

    se ajusta el componente

--- src/utils/DivisorLine/DivisorLine.tsx
+++ src/utils/DivisorLine/DivisorLine.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const DivisorLine = ({ color = 'white' }) => {
     return (
-        <div className="relative my-8 w-20">
+        <div className="relative my-8 w-20 mx-auto md:mx-0">
             <div className="absolute inset-0 flex items-center">
                 <div className={`w-full border-t-2 border-[${color}]`}></div>
+            </div>
+            <div className="relative flex justify-center">
+                <span className="px-2 bg-white text-gray-500"></span>
             </div>
         </div>
     );
